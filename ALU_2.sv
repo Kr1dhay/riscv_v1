78973178d1c7855463fac10c28ac8b838417390b
Kridhay <78971881+Kr1dhay@users.noreply.github.com>
2022-11-17 11:54:55 +0000

    alu started

---
+++ ALU_2.sv
@@ -0,0 +1,9 @@
+module alu_2 (
+    input logic [31:0] aluOp1,
+    input logic [31:0] aluOp2,
+    input logic [2:0] aluCTR,
+    output logic [31:0] sum,
+    output logic [31:0] eq
+)
+
+endmodule
