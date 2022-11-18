2390623bea30a6c547e517407ccf731e5b2df913
AEMLL <aem21@ic.ac.uk>
2022-11-18 13:16:50 +0000

    Chnaged CU logic from switch case to bit-decoding

--- R3/cu.sv
+++ R3/cu.sv
@@ -5,6 +5,7 @@ module cu #(
     input logic                     EQ,         //high for OpA == OpB
     input logic [OP_WIDTH-1:0]      Op,         //Instruction_word[6:0]
     input logic [2:0]               funct3,     //Instruction_word[14:12]
+    input logic [6:0]               funct7,     //Instruction_word[31:25]
     output logic                    RegWrite,   //Write enable for the register file
     output logic [CTRL_WIDTH-1]     ALUctrl,    //Configures ALU for required operation
     output logic                    ALUsrc,     //Selects register or immediate operand 
@@ -12,26 +13,27 @@ module cu #(
     output logic                    PCsrc       //Selects PC addressing between PC:=PC+4 and PC:=Imm32
 );
 
-//RegWrite LOGIC
+/*RegWrite LOGIC:   only certain types of instructions write to regFiles, e.g. U-type do not.
+                    The true cases are written but the false are given value 0 implicitly by default case*/
 always_comb begin
     case (Op)
         //I-type
         7'd3: RegWrite = 1'b1; 
         7'd19: RegWrite = 1'b1;
 
+        //R-type
+        7'd51: RegWrite = 1'b1;
+
         //U-type
         7'd23: RegWrite = 1'b1;
         7'd55: RegWrite = 1'b1;
-
-        //B-type
-        7'd99: RegWrite = 1'b0;
 
         default: RegWrite = 1'b0;
     endcase
 end
 
 
-//ALUsrc LOGIC
+//ALUsrc LOGIC: I-type instructions will use immediate operand (1), but others will have 0
 always_comb begin
     case (Op)
         //I-type
@@ -39,29 +41,31 @@ always_comb begin
         7'd19: ALUsrc = 1'b1;
         7'd103: ALUsrc = 1'b1;
 
-        //R-type
-        7'd51: ALUsrc = 1'b1;
-
         default: ALUsrc = 1'b0;
     endcase
 end
 
-//ALUctrl LOGIC
+/*ALUctrl LOGIC:    this 3-bit control input has been decoded by mapping each bit from funct3 to ALUctrl with
+                    a boolean expression.*/
 always_comb begin
-    OpDC5 = {Op[6],Op[4:0]};
-    case (OpDC5)
-        //R-type
-        7'd19:  case (funct3)
-                    3'b000: ALUctrl = 3'b0;
-                    //3'b001: ALUctrl = 3';
-                    3'b010: ALUctrl = 3'b101;
-                    3'b011: ALUctrl = 3'b101;
-                    //3'b100:
-                    //3'b101: ALU
-                    3'b110: ALUctrl = 3'b011;
-                    3'b111: ALUctrl = 3'b000;
-                    default: ALUctrl = 3'b0;
-                endcase
+
+    case (Op)
+        //R-type & I-type arithmetic and logic operations
+        7'd19:  begin
+                ALUctrl[2] = ~(funct3[2]) & funct3[1];
+                ALUctrl[1] = funct3[2] & funct3[1];
+                ALUctrl[0] = ~(funct3[2])&funct3[1] | funct3[1] & funct3[0] | ~(funct3) 
+                end
+
+        7'd51:  begin //
+                ALUctrl[2] = ~(funct3[2]) & funct3[1];
+                ALUctrl[1] = funct3[2] & funct3[1];
+                ALUctrl[0] = ~(funct3[2])&funct3[1] | funct3[1] & funct3[0] | ~(funct3) & (funct3 ==  7'h20);
+                end
+
+        //S-type DONT CARE
+        //B-type DONT CARE
+        //U-type add
 
         default: ALUctrl = 3'b0;
     endcase
--- R3/rom.sv
+++ R3/rom.sv
@@ -1,11 +1,12 @@
 module ROM #(
-    parameter   WIDTH = 32,    
+    parameter   ADDRESS_WIDTH = 32,
+                DATA_WIDTH = 8    
 )(
     input logic [WIDTH-1:0] A,
-    output logic [WIDTH-1:0] RD
+    output logic [(4*WIDTH)-1:0] RD
 );
 
-logic [WIDTH-1:0] rom_array [2**(WIDTH-2)-1:0]; //Byte addressing means that first 2 bits of address are not considered
+logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0]; //Byte addressing means that first 2 bits of address are not considered
 
 initial begin
     $display("Loading rom.");
@@ -14,7 +15,7 @@ end;
 
 always_comb begin
     // output is asynchronous
-    RD = rom_array[A];
+    RD = {rom_array[A+3], rom_array[A+2], rom_array[A+1], rom_array[A]};
 end
 
 endmodule
