bd3fb157aa833309765e662e11dde0c0dbef9bb1
ChangLiu <98478845+ChangLiu2022@users.noreply.github.com>
2022-11-18 11:56:00 +0000

    Add files via upload

---
+++ R4/riscv_tb.cpp
@@ -0,0 +1,35 @@
+#include "Vcounter.h" //modify
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc, char **argv , char **env) {
+    long i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    Vcounter* top= new Vcounter; //modify
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("counter.vcd"); //modify
+
+    top->clk = 1;
+    top->rst = 1;
+    
+    for(i=0;i<500000;i++){
+        for (clk = 0; clk<2; clk++){
+            tfp->dump(2*i+clk);
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        top->rst = (i==50);//reset here, modify
+
+        if (Verilated::gotFinish()) exit(0);
+    }
+
+
+    tfp->close();
+    exit(0);
+}
