4daa47326388821ebf6dbf7efa69d4e4db215674
ChangLiu <98478845+ChangLiu2022@users.noreply.github.com>
2022-11-25 10:53:18 +0000

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,16 +1,56 @@
-`include "R2/alu.sv"
-`include "R2/mux.sv"
-`include "R2/regFile.sv"
-`include "R3/cu.sv"
-`include "R3/ext32.sv"
-`include "R3/rom.sv"
-
 module top(
-    input wire clk,
-    input wire rst,
-    output wire a0
+    input logic clk,
+    input logic rst,
+    output logic a0
 );
 
+    logic PCsrc,
+    logic [31:0] ImmOp,
+    logic [31:0] PC,
+    logic [4:0] ad1,
+    logic [4:0] ad2,
+    logic [4:0] ad3,
+    logic we3,
+    logic aluSrc,
+    logic [2:0] aluCTR,
+    logic eq,
+    logic [1:0] immSrc
 
+program_counter pcounter(
+    .clk (clk),
+    .rst (rst),
+    .PCsrc (PCsrc),
+    .ImmOp (ImmOp),
+    .PC (PC)
+)
+
+control ctr(
+    .A (PC),
+    .EQ (eq),
+    .immSrc (immSrc),
+    .rs1 (ad1),
+    .rs2 (ad2),
+    .rd (ad3),
+    .RegWrite (we3),
+    .ALUctrl (aluCTR),
+    .ALUsrc (aluSrc),
+    .ImmSrc (immSrc),
+    .PCsrc (PCsrc),
+    .ImmOp (ImmOp)
+)
+
+red alu(
+    .clk (clk),
+    .rst (rst),
+    .a0 (a0),
+    .ad1 (ad1),
+    .ad2 (ad2),
+    .ad3 (ad3),
+    .we3 (we3),
+    .aluSrc (aluSrc),
+    .aluCTR (aluCTR),
+    .eq (eq),
+    .immOp (ImmOp)
+)
 
 endmodule
